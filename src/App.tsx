10d49d78e3a35407b67cd5c82ca38f45a155b89f
Mer Moyano <moyanomariamercedes@gmail.com>
2024-01-30 00:35:59 -0300

    Remove redundant state

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,19 @@ import { useState } from 'react'
 import { Board } from './Board'
 
 const Game = () => {
-  const [isXNext, setIsXNext] = useState(true)
   const [history, setHistory] = useState([new Array(9).fill(null)])
   const [currentMove, setCurrentMove] = useState(0)
+  const isXNext = currentMove % 2 === 0
   const currentCells = history[currentMove]
 
   const playHandler = (cells: string[]) => {
     const nextHistory = [...history.slice(0, currentMove + 1), cells]
     setHistory(nextHistory)
     setCurrentMove(nextHistory.length - 1)
-    setIsXNext(!isXNext)
   }
 
   const jumpToMove = (move: number) => {
     setCurrentMove(move)
-    setIsXNext(move % 2 === 0)
   }
 
   const moves = history.map((cells, i) => {
